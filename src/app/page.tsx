19e5d337eab0c02dea1d4dd287b455395267a019
eriickz <ericrodmon@gmail.com>
2024-06-20 02:37:51 -0400

    Added home header section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,53 @@
+import HomePromo from "@/assets/images/home/promo.jpg"
+import Image from "next/image";
+import Logo from "@/components/Logo"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import Link from "next/link";
+import Lang from "@/components/icons/Lang"
+
 export default function Home() {
   return (
     <main>
-    
+      <div className="relative overflow-hidden h-[700px] bg-home-promo-pattern bg-home-overlay">
+        <Image
+          alt="Home Promo"
+          src={HomePromo}
+          fill
+          quality={100}
+          placeholder="blur"
+          sizes="100vw"
+          className="z-[-1] scale-125 translate-y-[-12.2%] object-cover"
+        />
+        <div className="w-[66.6%] mx-auto">
+          <header className="flex items-center px-12 py-6">
+            <div className="w-[148px]">
+              <Logo />
+            </div>
+            <div className="flex gap-6 items-center h-8 ml-auto">
+              <Select value="English">
+                <SelectTrigger className="text-white justify-around border-home-select rounded p-2 gap-2 h-full font-NetflixSansRegular">
+                  <Lang />
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="English">
+                    English
+                  </SelectItem>
+                  <SelectItem value="Spanish">
+                    Spanish
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+              <Link
+                href="/"
+                className="flex items-center bg-primary text-white px-4 text-sm font-NetflixSansSemiBold w-full h-full rounded-md"
+              >
+                Sign In
+              </Link>
+            </div>
+          </header>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,56 @@
+import type { Config } from "tailwindcss"
+
+const config = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        NetflixSansRegular: ["var(--netflix-sans_400)"],
+        NetflixSansSemiBold: ["var(--netflix-sans_500)"],
+        NetflixSansBold: ["var(--netflix-sans_900)"],
+      },
+      colors: {
+        primary: "#e50914",
+        "home-select": "#ffffff66",
+        "home-overlay": "rgba(0, 0, 0, .4)"
+      },
+      scale: {
+        125: "1.25"
+      },
+      backgroundImage: {
+        "home-promo-pattern": "linear-gradient(to top, rgba(0, 0, 0, 0.8) 0, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%)"
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
