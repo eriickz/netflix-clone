8bedfdf99714fb8924b8c602c7c41db1cf09547c
eriickz <ericrodmon@gmail.com>
2024-08-08 00:14:16 -0400

    Fixed layout issues (3)

--- src/app/browse/app/App.tsx
+++ src/app/browse/app/App.tsx
@@ -2,22 +2,34 @@ import HeroImage from "./components/hero/HeroImage";
 import Slider from "./components/slider/Slider";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Image from "next/image";
 
 const App = () => {
   return (
-    <main className="w-full bg-choose-profile-background">
-      <div className="absolute top-0 right-0 left-0 bottom-0 w-full h-5/6 bg-[url('/images/app-home-1.webp')] bg-no-repeat bg-cover z-1 bg-[50%] lg:h-[96%]">
-        <div className="absolute w-1/2 h-full bg-app-home-hero-vignette-gradient z-2"></div>
+    <main className="w-full">
+      <div className="h-[56.25vw] relative">
+        <Image alt="App Top Background Image" src="/images/app-home-1.webp" width={0} height={0} sizes="100vw" fill objectFit="cover" className="z-1" />
+        <div className="absolute bg-app-home-hero-vignette-gradient z-2 bottom-0 top-0 left-0 right-[26.09%]"></div>
       </div>
-      <Header/>
-      <HeroImage />
-      <div className="relative z-3 mt-32 gap-y-[72px]">
-        <div className="text-4xl text-white font-NetflixSansSemiBold">
-          <p className="mb-4 pl-[60px]">Top 10 Movies in Dominican Republic Today</p>
-          <Slider /> 
+      <div className="absolute top-0 bg-choose-profile-background">
+        <Header/>
+        <HeroImage />
+        <div className="flex flex-col relative z-2 mt-32">
+          <div className="text-4xl text-white font-NetflixSansSemiBold">
+            <p className="mb-4 pl-[60px]">Top 10 Movies in Dominican Republic Today</p>
+            <Slider /> 
+          </div>
+          <div className="pt-[60px] text-4xl text-white font-NetflixSansSemiBold">
+            <p className="mb-4 pl-[60px]">Top 10 Movies in Dominican Republic Today</p>
+            <Slider /> 
+          </div>
+          <div className="pt-[60px] text-4xl text-white font-NetflixSansSemiBold">
+            <p className="mb-4 pl-[60px]">Top 10 Movies in Dominican Republic Today</p>
+            <Slider /> 
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
     </main>
   )
 }
